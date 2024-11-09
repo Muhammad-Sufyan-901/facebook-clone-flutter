580dd55e8a0c579498171c555294255230a79b92
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2024-11-09 20:25:37 +0800

    feat : reinitialize main app with provider and env variables

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:accessibility_tools/accessibility_tools.dart';
+import 'package:flutter_boilerplate/app/routes/app_routes.dart';
+
+import 'package:flutter_boilerplate/app/themes/app_themes.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Your Application Name',
+      theme: AppThemes.theme,
+      initialRoute: AppRoutes.initialRoute,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+      builder: (context, child) {
+        return AccessibilityTools(
+          child: child!,
+        );
+      },
+    );
+  }
+}
