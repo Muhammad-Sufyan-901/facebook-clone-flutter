5a5f5dbda7a99e02cb76ba112f6de48e565ee95e
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2024-11-09 20:26:08 +0800

    feat : adding basic default routes

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_boilerplate/app/views/screens/not_found_screen.dart';
+
+abstract class AppRoutes {
+  static const String initialRoute = '/';
+  static const String signInRoute = '/sign_in';
+  static const String signUpRoute = '/sign_up';
+  static const String forgotPasswordRoute = '/forgot_password';
+  static const String homeRoute = '/home';
+  static const String profileRoute = '/profile';
+  // Add your routes here
+
+  static List<String> get routes => [
+        initialRoute,
+        signInRoute,
+        signUpRoute,
+        forgotPasswordRoute,
+        homeRoute,
+        profileRoute,
+        // Add your routes here
+      ];
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case initialRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      case signInRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      case signUpRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      case forgotPasswordRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      case homeRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      case profileRoute:
+        return getScreen(
+          settings: settings,
+          screen: const Scaffold(),
+        );
+      default:
+        return getScreen(
+          settings: settings,
+          screen: const NotFoundScreen(),
+        );
+    }
+  }
+
+  static PageRoute<dynamic> getScreen({
+    required RouteSettings settings,
+    required Widget screen,
+  }) {
+    if (Platform.isIOS) {
+      return CupertinoPageRoute(
+        settings: settings,
+        builder: (BuildContext context) => screen,
+      );
+    } else {
+      return MaterialPageRoute(
+        settings: settings,
+        builder: (BuildContext context) => screen,
+      );
+    }
+  }
+}
