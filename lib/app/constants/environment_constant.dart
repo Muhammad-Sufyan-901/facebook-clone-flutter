02b9b1b65ac3093a920c6921fa980ae4913ea38c
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2025-02-24 18:21:11 +0800

    feat : adding some environment variables

--- lib/app/constants/environment_constant.dart
+++ lib/app/constants/environment_constant.dart
@@ -3,4 +3,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract class EnvironmentConstant {
   static String apiBaseURL =
       dotenv.env['API_BASE_URL'] ?? "YOUR_DEFAULT_API_BASE_URL";
+  static String apiKey =
+      dotenv.env['API_KEY'] ?? "YOUR_DEFAULT_API_KEY (SHOULD BE SECRET!)";
+  static String apiImageURL =
+      dotenv.env['API_IMAGE_URL'] ?? "YOUR_DEFAULT_API_IMAGE_URL";
 }
